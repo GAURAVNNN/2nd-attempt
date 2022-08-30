367f82606286d9115f71c26e07fcfc0c3520d0f1
gaurav <gauravnair101@gmail.com>
2022-08-30 10:33:37 +0530

    git

---
+++ mod 2.py
@@ -0,0 +1,4 @@
+def f1()
+    print('f1 m2')
+def f2()
+        print('f2 m2')
---
+++ mod1.py
@@ -0,0 +1,11 @@
+def sum1(a,b):
+
+    return a+b,a-b,a/b,a*b
+
+def sqr(no):
+    print("square of number is",no*no)
+
+    if no%2==0:
+       print("even")
+    else:
+        print('odd')
